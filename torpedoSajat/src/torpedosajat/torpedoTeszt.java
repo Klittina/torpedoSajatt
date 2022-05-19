595c750753d104821983575e2c26aab7bfb04c6a
Klittina <krisztike113@gmail.com>
2022-05-19 14:40:22 +0200

    első comment helye, torpedoTeszt létrehozása:main, tesztLoves metodus

---
+++ torpedoSajat/src/torpedosajat/torpedoTeszt.java
@@ -0,0 +1,10 @@
+package torpedosajat;
+
+public class torpedoTeszt {
+    public static void main(String[] args) {
+        
+    }
+    public String tesztLoves(int poz){
+        return null;
+    }
+}
