3eef3a11e2368f79a9d0a244cc2b566a9fb50c6e
Klittina <krisztike113@gmail.com>
2022-05-19 14:51:43 +0200

    harmadik commit, hajo létrehozása, assert talált

--- torpedoSajat/src/torpedosajat/torpedoTeszt.java
+++ torpedoSajat/src/torpedosajat/torpedoTeszt.java
@@ -2,9 +2,12 @@ package torpedosajat;
 
 public class torpedoTeszt {
     public static void main(String[] args) {
-        
+        tesztLoves(4);
     }
-    public String tesztLoves(int poz){
-        return null;
+    public static String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált");
+        return "";
     }
 }
